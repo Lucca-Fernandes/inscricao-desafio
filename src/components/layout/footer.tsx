3c6c7ff0bea8d3b0d030e698d6daaf70286b40b4
Lucca-Fernandes <lfdc212@gmail.com>
2026-01-23 14:52:11 -0300

    margin logo footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,9 +3,9 @@ import Logo from '../../public/logo.png';
 export function Footer() {
   return (
     <footer className="border-t">
-      <div className="container mx-auto px-4 py-6 flex items-center justify-between">
+      <div className="container mx-auto px-4 py-6 flex items-center justify-center">
         <img src={Logo.src} className="h-6" alt="Logo" />
-        <p className="text-sm text-muted-foreground">
+        <p className="text-sm text-muted-foreground ml-3">
           &copy; {new Date().getFullYear()} Projeto Desenvolve. Todos os direitos reservados.
         </p>
       </div>
