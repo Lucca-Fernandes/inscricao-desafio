6f6879a3913666377bd93b3d4a78d68579693476
Lucca-Fernandes <lfdc212@gmail.com>
2026-01-30 15:07:35 -0300

    ...

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,4 +1,3 @@
-"use client"
 
 import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
@@ -54,10 +53,10 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
+        IconLeft: ({ className, ...props }: { className?: string; [key: string]: any }) => (
           <ChevronLeft className={cn("h-4 w-4", className)} {...props} />
         ),
-        IconRight: ({ className, ...props }) => (
+        IconRight: ({ className, ...props }: { className?: string; [key: string]: any }) => (
           <ChevronRight className={cn("h-4 w-4", className)} {...props} />
         ),
       }}
