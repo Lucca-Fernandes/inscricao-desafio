d6ab33c1bdd4135c0121b12cc4e817475e4c18ef
Lucca-Fernandes <lfdc212@gmail.com>
2026-01-30 15:04:12 -0300

    ...

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -1,4 +1,4 @@
-"use client"
+
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "next-themes"
@@ -6,3 +6,4 @@ import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from "ne
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
+
